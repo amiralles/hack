c4310eb9a2483be854bcfd2386bb17a5e1462413
amiralles <amiralles.net@gmail.com>
2018-09-06 08:54:45 -0300

    Initial commit.

---
+++ hack.js
@@ -0,0 +1,23 @@
+/* hackernews js library. */
+function $(id) { return document.getElementById(id); }
+function byClass (el, cl) { return el ? el.getElementsByClassName(cl) : [] }
+function byTag (el, tg) { return el ? el.getElementsByTagName(tg) : [] }
+function allof (cl) { return byClass(document, cl) }
+function hasClass (el, cl) { var a = el.className.split(' '); return afind(cl, a) }
+function addClass (el, cl) { if (el) { var a = el.className.split(' '); if (!afind(cl, a)) { a.unshift(cl); el.className = a.join(' ')}} }
+function remClass (el, cl) { if (el) { var a = el.className.split(' '); arem(a, cl); el.className = a.join(' ') } }
+function html (el) { return el ? el.innerHTML : null; }
+function attr (el, name) { return el.getAttribute(name) }
+function tonum (x) { var n = parseFloat(x); return isNaN(n) ? null : n }
+function remEl (el) { el.parentNode.removeChild(el) }
+function posf (f, a) { for (var i=0; i < a.length; i++) { if (f(a[i])) return i; } return -1; }
+function apos (x, a) { return (typeof x == 'function') ? posf(x,a) : Array.prototype.indexOf.call(a,x) }
+function afind (x, a) { var i = apos(x, a); return (i >= 0) ? a[i] : null; }
+function acut (a, m, n) { return Array.prototype.slice.call(a, m, n) }
+function aeach (fn, a) { return Array.prototype.forEach.call(a, fn) }
+function arem (a, x) { var i = apos(x, a); if (i >= 0) { a.splice(i, 1); } return a; }
+function alast (a) { return a[a.length - 1] }
+function vis(el, on) { if (el) { on ? remClass(el, 'nosee') : addClass(el, 'nosee') } }
+function noshow (el) { addClass(el, 'noshow') }
+function elShow (el) { remClass(el, 'noshow') }
+function ind (el) { return (byTag(el, 'img')[0] || {}).width }
